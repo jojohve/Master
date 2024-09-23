799aac386bff99ef123f4f2b7a1a0148e937a20a
jojohve <joseph.laluna@outlook.it>
2024-09-23 19:47:25 +0200

    restart

---
+++ src/main/java/models/User.java
@@ -0,0 +1,46 @@
+package main.java.models;
+
+public class User {
+    private String username;
+    private String name;
+    private String password;
+
+    public User(String username, String name, String password) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+               "username='" + username + '\'' +
+               ", name='" + name + '\'' +
+               ", password='" + password + '\'' +
+               '}';
+    }
+}
